8a712098ce6f5db3a462f080cfdb94e5e2fc2a2b
Brendan-G5 <brendan.garvey@queensu.ca>
2020-06-09 17:46:06 +0200

    Setup page components created

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { SetUpComponent } from "./views/set-up/set-up.component";
+import { GamePlayComponent } from "./views/game-play/game-play.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: SetUpComponent },
+  { path: "play", component: GamePlayComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,9 +4,14 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./views/header/header.component";
+import { SetUpComponent } from './views/set-up/set-up.component';
+import { GamePlayComponent } from './views/game-play/game-play.component';
+import { HowToComponent } from './views/how-to/how-to.component';
+import { PlayerListComponent } from './components/player-list/player-list.component';
+import { AddPlayerComponent } from './components/add-player/add-player.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, SetUpComponent, GamePlayComponent, HowToComponent, PlayerListComponent, AddPlayerComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
